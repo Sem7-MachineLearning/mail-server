e5cfe1a2e456d3de26182e25bb2f464fb0c1db89
William Antoline <65105895+williamantoline@users.noreply.github.com>
2024-11-09 15:34:15 +0700

    feat (result): show prediction probability result

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 import numpy as np
+import matplotlib.pyplot as plt
+import string
+import random
+from fastapi.staticfiles import StaticFiles
 
 vectorizer = joblib.load('vectorizer.pkl')
 model = joblib.load('model.pkl') 
@@ -12,16 +17,51 @@ class InputData(BaseModel):
 
 app = FastAPI()
 
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'^subject:\s*', '', text)
+    text = re.sub(r'\W', ' ', text)
+    text = ' '.join([lemmatizer.lemmatize(word) for word in text.split() if word not in stop_words])
+    return text
+
+app.mount("/result", StaticFiles(directory="result"), name="result")
+
 @app.get("/status")
 def status():
     return {"status": "ok"}
 
 @app.post("/predict")
 def predict(input_data: InputData):
-    try:
-        features = vectorizer.transform([input_data.subject + " " + input_data.body])
-        prediction = model.predict(features)
-        result = bool(prediction[0])
-        return {"prediction": result}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    email = input_data.subject + " " + input_data.body
+    email = preprocess_text(email)
+    features = vectorizer.transform([email])
+    prediction_proba = model.predict_proba(features)[0]
+    prediction = model.predict(features)
+
+    image_name = ''.join(random.choices(string.ascii_letters, k=7))
+
+    labels = ['Not Spam', 'Spam']
+    colors = ['lightblue', 'salmon']
+    plt.figure(figsize=(6, 6))
+    plt.pie(prediction_proba, labels=labels, autopct='%1.1f%%', startangle=90, colors=colors)
+    plt.title("Probability Distribution for Email Classification")
+    plt.savefig("result/" + image_name + ".png")
+
+    result = bool(prediction[0])
+
+    return {
+        "prediction": result,
+        "image_name": image_name,
+    }
